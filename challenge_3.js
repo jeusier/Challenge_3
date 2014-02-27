1423217290c7836bd01e910f2660229c53575f30
jahsan87 <jahsan87@gmail.com>
2014-02-27 11:05:32 -1000

    passed both tests

--- challenge_3.js
+++ challenge_3.js
@@ -1,6 +1,19 @@
 exports.largestPrimeNumber = function(n){
   var primeNumber = 0;
   // do your work here
-  
+  var primes = [ ];
+  for (var i = 2; i < n; i++) {
+    if (n%i == 0) {
+        console.log("i'm in if");
+        console.log("i: "+i);
+        primes.push(i);
+        console.log(primes);
+        n = n/i;
+        console.log("n: "+n);
+       //return largestPrimeNumber(n);
+        }
+    } 
+  primes.push(n);
+  primeNumber = Math.max.apply(Math, primes);
   return primeNumber;
 };
